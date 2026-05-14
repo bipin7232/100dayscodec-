a97ea5cd5f31de08546f354b63e9854421f227f0
bipin7232 <phuyalbipin156@gmail.com>
2026-05-14 19:22:48 +0545

    day9

---
+++ Day9_Vector/RemoveDuplicate.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int arr[] = {1,2,1,2,3,3,};
+
+    int size = sizeof(arr)/sizeof(int);
+
+    int new_arr;
+
+    for(int i =0; i<size; i++){
+
+        for(int j= 0; j<size; j++){
+
+            
+                new_arr = arr[j];
+            
+        }
+    }
+
+    cout<<new_arr<<" ";
+
+    return 0;
+
+}
